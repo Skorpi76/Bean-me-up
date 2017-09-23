4f73c29a9e1b0a0b3ac0648dcb58429ddc79d06d
Eskender Memetov <skorpi76@gmail.com>
2017-09-22 23:50:35 -0400

    [Add] Entity Script, Engine Fire, [Modified] Projectily script
    Now those to whom Entity script applied has health(public var so heal
    can be changed) can get damage and die. Engine fire is done using
    particle system(will later try to do so it goes on only when you press
    W). And projectile now does damage to the Entity

---
+++ UnityProject/Bean Me Up/Assets/Scripts/Projectile.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+    private Rigidbody2D rb;
+    public float speed = 10;
+    public int damage = -10;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = transform.up * speed;
+    }
+
+    public virtual void OnCollisionEnter2D(Collision2D other)
+    {
+        OnCollision(other.gameObject);
+      
+        Destroy(this.gameObject);
+    }
+    protected virtual void OnCollision(GameObject obj)
+    {
+        Entity entity = obj.GetComponent<Entity>();
+        if (entity != null)
+        {
+            
+            entity.ModifyHealth(damage);
+
+        }
+    }
+}
