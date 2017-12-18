2455faf0412ac4a9379a2c12906f62d29e2d947c
Daniel Stokes <danielphilipstokes@gmail.com>
2017-12-18 12:21:38 -0500

    Spawner and Map Updates

---
+++ UnityProject/Bean Me Up/Assets/PrefabSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabSpawner : MonoBehaviour {
+
+	public GameObject prefab;
+	public float respawntime = 90;
+
+
+	// Use this for initialization
+	void Start () {
+		Spawn ();
+	}
+
+	void Spawn(){
+		GameObject prefabInstance = Instantiate (prefab, transform.position, transform.rotation) as GameObject;
+		prefabInstance.GetComponent<SpawnController> ().Respawner = this;
+	}
+
+	public void StartRespawn(){
+		StartCoroutine (RespawnTimer ());
+	}
+
+	IEnumerator RespawnTimer(){
+		yield return new WaitForSeconds(respawntime);
+		Spawn ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ UnityProject/Bean Me Up/Assets/SpawnController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour {
+
+	public PrefabSpawner Respawner;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void OnDestroy () {
+		Respawner.StartRespawn ();
+	}
+}
