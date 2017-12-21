643bf8b014cbfaf3af1ca5de8ac23385bb331a43
Daniel Stokes <danielphilipstokes@gmail.com>
2017-12-20 23:47:14 -0500

    AsteroidSprites

---
+++ UnityProject/Bean Me Up/Assets/Asteroid.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour {
+
+
+    public Sprite[] textureSprites;
+    public Sprite[] mapSprites;
+
+    public Color mapSpriteColor;
+    public LayerMask MapLayer;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ UnityProject/Bean Me Up/Assets/Editor/AsteroidHelper.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+[CustomEditor(typeof(Asteroid))]
+public class AsteroidHelper : Editor {
+
+    
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        Asteroid myScript = (Asteroid)target;
+        if (GUILayout.Button("Build Object"))
+        {
+            foreach (Transform t in myScript.transform) {
+                Debug.Log("another child");
+                if (t.gameObject.GetComponent<Collider2D>() != null)
+                {
+                    DestroyImmediate(t.gameObject.GetComponent<Collider2D>());
+                }
+                Sprite sp = t.GetComponent<SpriteRenderer>().sprite;
+
+                int i = 0;
+                foreach (Sprite sCheck in myScript.textureSprites) {
+                    if (sp == sCheck) {
+                        t.GetComponent<SpriteRenderer>().sprite = myScript.mapSprites[i];
+                        break;
+                    }
+                    i ++;
+                }
+
+                t.GetComponent<SpriteRenderer>().color = myScript.mapSpriteColor;
+
+                if (t.gameObject.layer != myScript.MapLayer) {
+                    t.gameObject.layer = myScript.MapLayer;
+                }
+            }
+        }
+    }
+}
